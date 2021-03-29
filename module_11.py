20f6fe6fa0caf3478ba280c6d14dba6f0137796a
EmmanuelAmula <emmanuelamula2016@gmail.com>
2021-03-29 03:07:07 -0400

    Create module_11.py

---
+++ module_11.py
@@ -0,0 +1,9 @@
+#
+# import pandas as pd
+#
+# df = pd.read_excel (r'G:\O7040\Documents\COVID19_03242020_ByCounty.xlsx')
+# print (df)
+
+# i have been getting multiple errors, i tried importing using openpyxl, xlrd and still got similar error message.
+
+
